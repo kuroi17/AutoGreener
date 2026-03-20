79b4bc484ff9ac2589ab18c9a7fa3d9b970dedfc
kuroi017 <painkira518@gmail.com>
2026-03-20 23:02:35 +0800

    iconReplacement + frontendURL

--- backend/index.js
+++ backend/index.js
@@ -16,7 +16,7 @@ const app = express();
 // Middleware
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL || "http://localhost:5173",
+    origin: process.env.FRONTEND_URL || "https://autogreener.onrender.com",
     credentials: true, // Allow cookies to be sent
   }),
 );
